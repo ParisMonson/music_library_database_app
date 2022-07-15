3bc808ecaf6854d774345a3b25d5d00d154c5e11
Paris Monson <parismonson@yahoo.com>
2022-07-15 13:23:36 +0100

    database_connection  updated

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -11,6 +11,12 @@ class DatabaseConnection
   # PG gem. We connect to 127.0.0.1, and select
   # the database name given in argument.
   def self.connect
+
+    if ENV['DATABASE_URL'] != nil
+      @connection = PG.connect(ENV['DATABASE_URL'])
+      return
+    end
+    
     if ENV['ENV'] == 'test'
       database_name = 'music_library_test'
     else
@@ -26,3 +32,21 @@ class DatabaseConnection
     @connection.exec_params(query, params)
   end
 end
+
+# file: lib/database_connection.rb
+
+# def self.connect
+#   # If the environment variable (set by Heroku)
+#   # is present, use this to open the connection.
+#   if ENV['DATABASE_URL'] != nil
+#     @connection = PG.connect(ENV['DATABASE_URL'])
+#     return
+#   end
+
+#   if ENV['ENV'] == 'test'
+#     database_name = 'music_library_test'
+#   else
+#     database_name = 'music_library'
+#   end
+#   @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+# end
